bce35bbf6f6baedf530abb9b71890eaaab53bde1
Tayyab Jadoon <tayyabkhan5228131@gmail.com>
2026-05-02 21:09:26 +0500

    Implement user model methods for password hashing and JWT generation

--- src/models/user.modles.js
+++ src/models/user.modles.js
@@ -15,6 +15,8 @@
 */
 
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema(
   {
@@ -74,6 +76,39 @@ const userSchema = new Schema(
     timestamps: true, // createdAt & updatedAt auto
   }
 );
+userSchema.pre("save",async function(next){
+  if(!this.isModified("password")) return next();
+  this.password=await bcrypt.hash(this.password,10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken =function(){
+  return jwt.sign(
+    {
+    _id:this._id,
+    username:this.username,
+    email:this.email
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+   expiresIn:process.env.EXPIRY
+  }
+)
+}
+userSchema.methods.generateRefreshToken =function(){
+  return jwt.sign(
+    {
+    _id:this._id,
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+   expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+  }
+)
+}
 
 // Export Model
 export const User = mongoose.model("User", userSchema);
