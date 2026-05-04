c179a6a81e1a41fcd33d87b886a0b226c736b1d7
Tayyab Jadoon <tayyabkhan5228131@gmail.com>
2026-05-04 15:17:28 +0500

    Add user registration functionality with file uploads and Cloudinary integration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs';
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if(!localFilePath) return null;
+        const response = cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto",
+        });
+        fs.unlinkSync(localFilePath);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+const deleteFromCloudinary = async(public_id)=>{
+    try{
+        const result = await cloudinary.uploader.destroy(public_id)
+        console.log("Deleted from cloudinary, public id:", publicId)
+    } catch (error) {
+        console.log("Error deleting from cloudinary:", error)
+        return null
+    }
+}
+export {
+    uploadOnCloudinary,
+    deleteFromCloudinary
+};
